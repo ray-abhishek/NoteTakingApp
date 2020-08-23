61cd61bf8a091d5c12fb4ce962aca1d3b6de3974
Abhishek Ray <apostate22@gmail.com>
2020-08-23 23:35:42 +0530

    Setup Header & Searhbox

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { toggleSidebar } from '../../Redux/action'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import SearchBox from './SearchBox'
+import { useDispatch, useSelector } from 'react-redux'
+
+
+export default function Header(){
+
+    const { activeTab } = useSelector(state => state)
+    const dispatcher = useDispatch()
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        console.log("Handling BARS Click")
+        dispatcher(toggleSidebar())
+    }
+
+    return (
+        <div style={container}>
+            <button style={buttonStyle} onClick={(e)=>{
+                handleClick(e)}}><FontAwesomeIcon icon={faBars} /></button>
+            <h2 style={titleStyle}>{activeTab.length>0 ? activeTab : "Keep"}</h2>
+            <SearchBox/>
+        </div>
+    )
+}
+
+const container = {
+    display : 'flex',
+    justifyContent : 'start'
+}
+
+const buttonStyle = {
+    marginLeft : '20px',
+    color : 'white',
+    backgroundColor : 'black',
+    border : '0px solid',
+    fontSize : '20px',
+    cursor : 'pointer',
+}
+
+const titleStyle = {
+    marginLeft : '17px',
+}
---
+++ src/Components/Header/SearchBox.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { getSearchResults, resetSearchDisplay } from '../../Redux/action'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { useDispatch } from 'react-redux'
+
+
+export default function SearchBox(){
+
+    const [ searchValue, setSearchValue ] = useState('')
+
+    const dispatcher = useDispatch()
+    const handleChange = async (e) => {
+        await setSearchValue(e.target.value)
+        dispatcher(getSearchResults(searchValue))
+    }
+
+    const handleClose = (e) => {
+        e.preventDefault()
+        setSearchValue("")
+        dispatcher(resetSearchDisplay())
+    }
+    return (
+        <div style={container}>
+        <input type="text" value={searchValue} onChange={(e)=>handleChange(e)} style={inputStyle} placeholder="Enter Keyword...">
+
+        </input>
+        {searchValue.length>0 && <FontAwesomeIcon icon={faTimes} style={closeIcon}  onClick={(e)=>handleClose(e)}/>}
+        </div>
+    )
+}
+
+const container = {
+    marginTop : '20px',
+    marginLeft : '50px',
+    position : 'relative'
+}
+
+const closeIcon = {
+    position : 'absolute',
+    zIndex : 2,
+    top : '10px',
+    right : '10px',
+    color : 'white',
+    cursor : 'pointer'
+}
+
+const inputStyle = {
+    fontSize : '1.3rem',
+    borderRadius : '10px',
+    backgroundColor : 'rgb(17, 36, 56)',
+    border : '0.8px solid',
+    padding : '5px',
+    color : 'white',
+}
