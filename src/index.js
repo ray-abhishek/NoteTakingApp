ce88105ea2dce9de936151dd56fbc890e5382715
Abhishek Ray <apostate22@gmail.com>
2020-08-23 23:32:02 +0530

    Setup Index.js with Redux store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 import store from './Redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
